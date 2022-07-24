aee6ca950025ac014a005320542742eab8b34ad3
eugene02876 <eugene02876@gmail.com>
2022-07-25 02:22:28 +0900

    카카오 로그인 구현
    카카오 로그인 구현을 하였습니다.

--- sinchonthon/sinchonthon/settings.py
+++ sinchonthon/sinchonthon/settings.py
@@ -165,4 +165,6 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'index' #로그인 후에 리디렉션 할 페이지
+ACCOUNT_LOGOUT_REDIRECT_URL = 'index' #로그아웃 후 리디렉션 할 페이지
+ACCOUNT_LOGOUT_ON_GET = True #로그아웃 버튼 클릭 시 자동 로그아웃
